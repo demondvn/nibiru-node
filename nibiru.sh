586c875f821e80baf94c20cb9a094eeaeb6d4e2c
MonPham <demondhunter93@gmail.com>
2023-03-01 09:26:02 +0700

    .

--- nibiru.sh
+++ nibiru.sh
@@ -3,6 +3,7 @@
 
 CHAIN_ID=nibiru-itn-1
 NODE_HOME=$HOME/.nibid
+nibid config keyring-backend test
 nibid init "$NIBIRU_NODENAME" --chain-id="$CHAIN_ID"
 echo 'export CHAIN_ID='\"${CHAIN_ID}\" >> $HOME/.bash_profile
 
@@ -15,16 +16,14 @@ pruning_keep_recent="100"
 pruning_keep_every="0"
 pruning_interval="10"
 sed -i 's|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $NODE_HOME/config/config.toml
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $NODE_HOME/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $NODE_HOME/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $NODE_HOME/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $NODE_HOME/config/app.toml
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025unibi\"/" $NODE_HOME/config/app.toml
-sed -i 's|enable =.*|enable = true|g' $NODE_HOME/config/config.toml
-sed -i 's|rpc_servers =.*|rpc_servers = "'$(curl -s https://networks.itn.nibiru.fi/$CHAIN_ID/rpc_servers)'"|g' $NODE_HOME/config/config.toml
-sed -i 's|trust_height =.*|trust_height = "'$(curl -s https://networks.itn.nibiru.fi/$CHAIN_ID/trust_height)'"|g' $NODE_HOME/config/config.toml
-sed -i 's|trust_hash =.*|trust_hash = "'$(curl -s https://networks.itn.nibiru.fi/$CHAIN_ID/trust_hash)'"|g' $NODE_HOME/config/config.toml
 
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' $HOME/.nibid/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001unibi"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.nibid/config/config.toml
 
 wget -O $HOME/.nibid/config/genesis.json https://raw.githubusercontent.com/Pa1amar/testnets/main/nibiru/nibiru-itn-1/genesis.json
 nibid tendermint unsafe-reset-all --keep-addr-book
