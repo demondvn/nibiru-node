be40252e14557e482ea029a7362330443a80dee9
MonPham <demondhunter93@gmail.com>
2023-03-02 23:19:17 +0700

    Create pricefeeder.sh

---
+++ pricefeeder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export FEEDER_MNEMONIC= "my pricefeeder 24 word mnemonic phrase goes here..."
+export CHAIN_ID="nibiru-itn-1"
+export GRPC_ENDPOINT="localhost:9090"
+export WEBSOCKET_ENDPOINT="ws://localhost:26657/websocket"
+export EXCHANGE_SYMBOLS_MAP='{ "bitfinex": { "ubtc:uusd": "tBTCUSD", "ueth:uusd": "tETHUSD", "uusdt:uusd": "tUSTUSD" }, "binance": { "ubtc:uusd": "BTCUSD", "ueth:uusd": "ETHUSD", "uusdt:uusd": "USDTUSD", "uusdc:uusd": "USDCUSD", "uatom:uusd": "ATOMUSD", "ubnb:uusd": "BNBUSD", "uavax:uusd": "AVAXUSD", "usol:uusd": "SOLUSD", "uada:uusd": "ADAUSD", "ubtc:unusd": "BTCUSD", "ueth:unusd": "ETHUSD", "uusdt:unusd": "USDTUSD", "uusdc:unusd": "USDCUSD", "uatom:unusd": "ATOMUSD", "ubnb:unusd": "BNBUSD", "uavax:unusd": "AVAXUSD", "usol:unusd": "SOLUSD", "uada:unusd": "ADAUSD" } }'
+export VALIDATOR_ADDRESS=$(nibid keys show wallet --bech val -a)
+while true
+do
+    /usr/local/bin/pricefeeder
+
+    # Sleep for 5 seconds before restarting
+    sleep 5
+done
